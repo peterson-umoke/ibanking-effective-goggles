168baffbc8726a0b743631545371c1f57a4f34a9
Peterson Umoke <umoke10@hotmail.com>
2021-06-20 15:58:01 +0100

    trying to add upload id card to the mix

--- core/app/Http/Controllers/Auth/RegisterController.php
+++ core/app/Http/Controllers/Auth/RegisterController.php
@@ -80,7 +80,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param array $data
-     * @return \App\User
+     * @return User|\App\User|\Illuminate\Database\Eloquent\Model
      */
     protected function create(array $data)
     {
